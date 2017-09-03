9df1c53d6a325c87263c12ad137b54ab006de37f
Jake Carlson <jrcarlson@smu.edu>
2017-09-03 15:14:54 -0500

    Fixes aws cli call

--- bin/push-latest.sh
+++ bin/push-latest.sh
@@ -2,7 +2,7 @@
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     pip install --user awscli
     export PATH=$PATH:$HOME/.local/bin
-    eval $(awc ecr get-login --region us-west-2)
+    eval $(aws ecr get-login --region us-west-2)
     docker-compose build -f ./config/production/docker-compose.yml
     docker tag maestro/web:latest 272378791211.dkr.ecr.us-west-2.amazonaws.com/maestro/web:latest
     docker push 272378791211.dkr.ecr.us-west-2.amazonaws.com/maestro/web:latest
