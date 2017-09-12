c8e29298639b90d862df5cc80e383740e0102251
18jelias <jacquie19_e@yahoo.com>
2017-09-12 15:03:57 -0500

    Runs with docker

--- bin/up.sh
+++ bin/up.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 docker-compose -f $1 up -d
-docker-compose -f $1 exec maestro-web npm run dev
---
+++ src/public/app/app.js
@@ -0,0 +1,29 @@
+import App from './App.vue';
+import router from './router/index.js';
+
+/* Example code only! */
+Parse.initialize("maestro", "orange");
+Parse.serverURL = 'http://maestro.dev:3308/parse';
+
+var TestObject = Parse.Object.extend("TestObject");
+var obj = new TestObject();
+
+var app = new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App },
+    mounted: function () {
+        obj.save({
+            name: 'It works!'
+        },{
+            success: (obj) => {
+                console.log('save success');
+            },
+            error: (obj, err) => {
+                console.error('save error:');
+                console.error(err);
+            }
+        });
+    }
+});
