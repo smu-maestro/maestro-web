6c73b6e5a5004e5227d9885466e72308bf43db80
Andrew Ksendzoff <dacoconuttman@gmail.com>
2017-09-18 22:35:21 -0500

    stuff and junk

--- src/backend/config_beta.js
+++ src/backend/config_beta.js
@@ -6,16 +6,32 @@ Parse.serverURL = 'http://maestro-backend:3308/parse';
 Parse.masterKey = 'orange';
 
 var load = () => {
-  const lessons = requireDir('./lessons/demo');
-  Object.values(lessons).forEach((lesson) => {
-    var parseLesson = Parse.Object.extend('lesson');
-    console.log(lesson);
+  var parseLesson = Parse.Object.extend('lesson');
+  var parseQuiz = Parse.Object.extend('quiz');
+  var lessons = requireDir('../lessons/demo');
+  for (lessonName in lessons) {
+    let lesson = lessons[lessonName];
     var newLesson = new parseLesson();
-    for(prop in lesson){
+    for (prop in lesson) {
       newLesson.set(prop, lesson[prop]);
     }
-    newLesson.save();
-  });
+    newLesson.save()
+      .then((savedLesson) => {
+        var quiz = require(`./quiz/${lessonName}.json`);
+        var newQuiz = new parseQuiz();
+        for (prop1 in quiz) {
+          newQuiz.set(prop1, quiz[prop1]);
+        }
+        newQuiz.set('lessonId', savedLesson.id);
+        return newQuiz.save();
+      })
+      .then((savedQuiz) => {
+        console.log('saved quiz fam');
+      })
+      .catch((err) => {
+        console.error('shits broke fam');
+      });
+  }
 }
 
 load();
