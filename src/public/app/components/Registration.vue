ffe0bb4765a3713d00988008d8a49e0cb26e0a30
Jake Carlson <jrcarlson@smu.edu>
2017-09-19 02:54:22 -0500

    Someone will want these changes

--- src/public/app/components/Registration.vue
+++ src/public/app/components/Registration.vue
@@ -5,13 +5,13 @@
 			  <label for="user_email">
 				  Email
 			  </label>
-			  <input type="text" id="user_email" name="user_email" placeholder="email@example.com" v-model="userEmail">
+			  <input type="text" name="user_email" placeholder="email@example.com" v-model="userEmail">
 		  </div>
 		  <div>
 			  <label for="user_password">
 				  Password
 			  </label>
-			  <input type="password" id="user_password" name="user_password" placeholder="password" v-model="userPassword">
+			  <input type="password" name="user_password" placeholder="password" v-model="userPassword">
 		  </div>
 		  <button type="submit" @click="register">
 			  Create Account
